430283a5c1f0239c3d27ff135804f44bfaede7c9
Anuja-More-HFN <anuja.more@volunteer.heartfulness.org>
2023-10-11 12:07:54 +0530

    authcallback to check if user exists in db or it new user then add to db

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import Navbar from '@/components/ui/Navbar'
+import Providers from '@/components/ui/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,10 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='light'>
+      <Providers>
       <body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>
         <Navbar />
         {children}
         </body>
+        </Providers>
     </html>
   )
 }
